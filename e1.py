0afff881fb9b4528e474b9eeb7d773d467f58dcc
s1300088 <s1300088@u-aizu.ac.jp>
2023-06-09 13:03:01 +0900

    Greetings added

--- e1.py
+++ e1.py
@@ -1,11 +1,3 @@
-import random
-
-a = random.randint(1, 6)
-b = random.randint(1, 6)
-
-print("Rolling dice...")
-
-print("Die 1: ", a)
-print("Die 2: ", b)
-
-print("Total value: ", (a+b))
+print("What is your name?")
+name = input()
+print("Hello, "+ name + "!")
